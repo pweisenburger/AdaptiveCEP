5a314164191ea9d1894a5ab0a4a87ed38d31566f
Lucas Bärenfänger <mail@scalarookie.com>
2016-11-17 15:28:00 +0100

    Add working implementation of joins

---
+++ src/main/scala/com/scalarookie/eventscala/actors/PublisherActor.scala
@@ -0,0 +1,45 @@
+package com.scalarookie.eventscala.actors
+
+import java.util.concurrent.TimeUnit
+import scala.util.Random
+import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.{Actor, ActorRef}
+import com.scalarookie.eventscala.actors.PublisherActor._
+
+object PublisherActor {
+
+  case object Subscribe
+  case object Unsubscribe
+
+}
+
+trait PublisherActor extends Actor {
+
+  var subscribers: Set[ActorRef] =
+    scala.collection.immutable.Set.empty
+
+  override def receive: Receive = {
+    case Subscribe =>
+      this.subscribers = this.subscribers + sender
+    case Unsubscribe =>
+      this.subscribers = this.subscribers - sender
+  }
+
+}
+
+case class RandomEventPublisher[T](name: String, createEventFromId: Int => T) extends PublisherActor {
+
+  def publish(id: Int): Unit = {
+    val t = createEventFromId(id)
+    this.subscribers.foreach(_ ! t)
+    println(s"Published in stream $name: $t")
+    context.system.scheduler.scheduleOnce(
+      delay = FiniteDuration(Random.nextInt(5000), TimeUnit.MILLISECONDS),
+      runnable = new Runnable { def run() = publish(id + 1) }
+    )
+  }
+
+  this.publish(0)
+
+}
---
+++ src/main/scala/com/scalarookie/eventscala/demos/Main.scala
@@ -0,0 +1,26 @@
+package com.scalarookie.eventscala.demos
+
+import akka.actor.{ActorSystem, Props}
+import com.scalarookie.eventscala.actors._
+import com.scalarookie.eventscala.caseclasses._
+
+object Main extends App {
+
+  val actorSystem = ActorSystem()
+
+  val publisherA = actorSystem.actorOf(Props(RandomEventPublisher("A", id => Event2[Integer, String](id, "X"))), "A")
+  val publisherB = actorSystem.actorOf(Props(RandomEventPublisher("B", id => Event2[Integer, Character](id, 'Y'))), "B")
+  val publisherC = actorSystem.actorOf(Props(RandomEventPublisher("C", id => Event1[Integer](id))), "C")
+
+  val publishers = Map("A" -> publisherA, "B" -> publisherB, "C" -> publisherC)
+
+  val join: Join = Join(
+    Stream2[Integer, String]("A"), LengthBatch(3),
+    Join(
+      Stream2[Integer, Character]("B"), LengthBatch(3),
+      Stream1[Integer]("C"), LengthBatch(3)),
+    LengthBatch(3))
+
+  val joinActor = actorSystem.actorOf(Props(new JoinActor(join, publishers, None)), "root")
+
+}
