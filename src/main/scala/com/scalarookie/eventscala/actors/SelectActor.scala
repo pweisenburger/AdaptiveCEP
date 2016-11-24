648025df2571129e31fd722aee0cef5886661dcf
Lucas Bärenfänger <mail@scalarookie.com>
2016-11-24 17:31:54 +0100

    Factor out Esper-related code in trait `EsperEngine`

--- src/main/scala/com/scalarookie/eventscala/actors/SelectActor.scala
+++ src/main/scala/com/scalarookie/eventscala/actors/SelectActor.scala
@@ -4,27 +4,24 @@ import akka.actor.{Actor, ActorRef, Props}
 import com.espertech.esper.client._
 import com.scalarookie.eventscala.caseclasses._
 
-class SelectActor(select: Select, publishers: Map[String, ActorRef], root: Option[ActorRef]) extends Actor {
+class SelectActor(select: Select, publishers: Map[String, ActorRef], root: Option[ActorRef]) extends Actor with EsperEngine {
 
   /* TODO */ println(s"Node `${self.path.name}` created; representing `$select`.")
+
+  val actorName: String = self.path.name
+  override val serviceProviderUri: String = actorName
 
   val subquery: Query = select.subquery
   val elementIds: List[Int] = select.elementIds
 
-  val configuration = new Configuration
-  lazy val serviceProvider = EPServiceProviderManager.getProvider(s"${self.path.name}", configuration)
-  lazy val runtime = serviceProvider.getEPRuntime
-  lazy val administrator = serviceProvider.getEPAdministrator
-
-  val subqueryElementClasses: Array[java.lang.Class[_]] = Query.getArrayOfClassesFrom(subquery)
+  val subqueryElementClasses: Array[Class[_]] = Query.getArrayOfClassesFrom(subquery)
   val subqueryElementNames: Array[String] = (1 to subqueryElementClasses.length).map(i => s"e$i").toArray
 
-  configuration.addEventType("subquery", subqueryElementNames, subqueryElementClasses.asInstanceOf[Array[AnyRef]])
+  addEventType("subquery", subqueryElementNames, subqueryElementClasses)
 
   val elementIdsEpl: String = elementIds.map(i => s"sq.e$i").mkString(", ")
 
-  val eplStatement: EPStatement = administrator.createEPL(
-    s"select $elementIdsEpl from subquery as sq")
+  val eplStatement: EPStatement = createEplStatement(s"select $elementIdsEpl from subquery as sq")
 
   eplStatement.addListener(new UpdateListener {
     override def update(newEvents: Array[EventBean], oldEvents: Array[EventBean]): Unit = {
@@ -38,22 +35,22 @@ class SelectActor(select: Select, publishers: Map[String, ActorRef], root: Optio
   val subqueryActor: ActorRef = subquery match {
     case stream: Stream => context.actorOf(Props(
       new StreamActor(stream, publishers, Some(root.getOrElse(self)))),
-      s"${self.path.name}-stream")
+      s"$actorName-stream")
     case join: Join => context.actorOf(Props(
       new JoinActor(join, publishers, Some(root.getOrElse(self)))),
-      s"${self.path.name}-join")
+      s"$actorName-join")
     case select: Select => context.actorOf(Props(
       new SelectActor(select, publishers, Some(root.getOrElse(self)))),
-      s"${self.path.name}-select")
+      s"$actorName-select")
     case filter: Filter => context.actorOf(Props(
       new FilterActor(filter, publishers, Some(root.getOrElse(self)))),
-      s"${self.path.name}-filter")
+      s"$actorName-filter")
   }
 
   override def receive: Receive = {
     case event: Event =>
       if (sender == subqueryActor) {
-        runtime.sendEvent(Event.getArrayOfValuesFrom(event), "subquery")
+        sendEvent("subquery", Event.getArrayOfValuesFrom(event))
       }
   }
 
