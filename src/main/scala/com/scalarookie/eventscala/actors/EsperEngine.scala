c23240005c15b09965457bc21cb9ca3c48cdb383
Lucas Bärenfänger <mail@scalarookie.com>
2016-11-24 17:31:54 +0100

    Factor out Esper-related code in trait `EsperEngine`

---
+++ src/main/scala/com/scalarookie/eventscala/actors/EsperEngine.scala
@@ -0,0 +1,24 @@
+package com.scalarookie.eventscala.actors
+
+import com.espertech.esper.client._
+
+trait EsperEngine {
+
+  val serviceProviderUri: String
+
+  val configuration = new Configuration
+  // Using `lazy val`s here is inspired by https://www.lightbend.com/activator/template/akka-with-esper
+  lazy val serviceProvider = EPServiceProviderManager.getProvider(serviceProviderUri, configuration)
+  lazy val runtime = serviceProvider.getEPRuntime
+  lazy val administrator = serviceProvider.getEPAdministrator
+
+  def addEventType(eventTypeName: String, elementNames: Array[String], elementClasses: Array[Class[_]]): Unit =
+    configuration.addEventType(eventTypeName, elementNames, elementClasses.asInstanceOf[Array[AnyRef]])
+
+  def createEplStatement(eplString: String): EPStatement =
+    administrator.createEPL(eplString)
+
+  def sendEvent(eventTypeName: String, eventAsArray: Array[AnyRef]) =
+    runtime.sendEvent(eventAsArray, eventTypeName)
+
+}
