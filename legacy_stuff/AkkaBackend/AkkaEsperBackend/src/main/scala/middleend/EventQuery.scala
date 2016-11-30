7439ab67fe49bee1ac088d0ccd605e86650fa9ca
Lucas Bärenfänger <mail@scalarookie.com>
2016-11-30 18:04:03 +0100

    Remove `legacy_stuff` folder

--- legacy_stuff/AkkaBackend/AkkaEsperBackend/src/main/scala/middleend/EventQuery.scala
+++
@@ -1,8 +0,0 @@
-package middleend
-
-sealed trait EventQuery
-
-case class PrimitiveQuery(name: String) extends EventQuery
-case class SequenceQuery(qs: List[EventQuery]) extends EventQuery
-case class AndQuery(q0: EventQuery, q1: EventQuery) extends EventQuery
-case class OrQuery(q0: EventQuery, q1: EventQuery) extends EventQuery
