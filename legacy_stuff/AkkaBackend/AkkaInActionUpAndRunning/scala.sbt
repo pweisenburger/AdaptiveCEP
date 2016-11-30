848022d01101f7de430a2607602e41c64a12f3aa
Lucas Bärenfänger <mail@scalarookie.com>
2016-11-30 18:04:03 +0100

    Remove `legacy_stuff` folder

--- legacy_stuff/AkkaBackend/AkkaInActionUpAndRunning/scala.sbt
+++
@@ -1,9 +0,0 @@
-scalaVersion := "2.11.8"
-
-scalacOptions ++= Seq(
-  "-deprecation",
-  "-unchecked",
-  "-Xfatal-warnings",
-  "-feature",
-  "-language:_"
-)
--- legacy_stuff/AkkaBackend/SnippetDynamicallyCreatingChildren/src/main/scala/Messages.scala
+++
@@ -1,9 +0,0 @@
-import akka.actor.ActorRef
-
-object Messages {
-
-  case class Created(childNodeId: Int, childNodeRef: ActorRef)
-  case class Greet(childNodeId: Int)
-  case object Hi
-
-}
