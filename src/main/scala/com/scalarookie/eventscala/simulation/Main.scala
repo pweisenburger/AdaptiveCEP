6f88920f989fe80a08dfd6e88f77b771bef0a782
Lucas Bärenfänger <mail@scalarookie.com>
2017-02-10 20:21:26 +0100

    Add second sample query to the simulation

--- src/main/scala/com/scalarookie/eventscala/simulation/Main.scala
+++ src/main/scala/com/scalarookie/eventscala/simulation/Main.scala
@@ -23,7 +23,7 @@ object Main extends App {
     "C" -> publisherC,
     "D" -> publisherD)
 
-  val sampleQuery: Query3[Either[Int, String], Either[Int, X], Either[Float, X]] =
+  val query1: Query3[Either[Int, String], Either[Int, X], Either[Float, X]] =
     stream[Int]("A")
     .join(
       stream[Int]("B"),
@@ -40,17 +40,33 @@ object Main extends App {
     .and(stream[Float]("C"))
     .or(stream[String]("D"))
 
+  val query2: Query4[Int, Int, Float, String] =
+    stream[Int]("A")
+    .and(stream[Int]("B"))
+    .join(
+      sequence(
+        noReqStream[Float]("C") -> noReqStream[String]("D"),
+        frequency > ratio(1.instances, 5.seconds) otherwise { (nodeData) => println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!") }),
+      slidingWindow(3.seconds),
+      slidingWindow(3.seconds),
+      latency < timespan(1.milliseconds) otherwise { (nodeData) => println(s"PROBLEM:\tEvents reach node `${nodeData.name}` too slow!") })
+
+
   val graph: ActorRef = GraphFactory.create(
     actorSystem =             actorSystem,
-    query =                   sampleQuery,
+    query =                   query1, // Alternatively: `query2`
     publishers =              publishers,
     frequencyMonitorFactory = AveragedFrequencyMonitorFactory (interval = 15, logging = true),
     latencyMonitorFactory =   PathLatencyMonitorFactory       (interval =  5, logging = true),
     createdCallback =         () => println("STATUS:\tGraph has been created."))(
     eventCallback =           {
-      case (Left(i1), Left(i2), Left(f)) =>  println(s"COMPLEX EVENT:\tEvent3($i1,$i2,$f)")
-      case (Right(s), _, _)              =>  println(s"COMPLEX EVENT:\tEvent1($s)")
-      case _                             =>  // This is necessary to avoid warnings about non-exhaustive `match`.
+      // Callback for `query1`
+      case (Left(i1), Left(i2), Left(f)) => println(s"COMPLEX EVENT:\tEvent3($i1,$i2,$f)")
+      case (Right(s), _, _)              => println(s"COMPLEX EVENT:\tEvent1($s)")
+      // Callback for `query2`
+      // case (i1, i2, f, s)             => println(s"COMPLEX EVENT:\tEvent4($i1, $i2, $f,$s)")
+      // This is necessary to avoid warnings about non-exhaustive `match`.
+      case _                             =>
     })
   
 }
