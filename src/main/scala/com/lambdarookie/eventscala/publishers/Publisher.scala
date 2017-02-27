05f3382d7b065c3815a8645c2b15ae7cea04ca85
Lucas Bärenfänger <mail@lucasbaerenfaenger.com>
2017-02-27 01:23:15 +0100

    Rename packages from `com.scalarookie...` to `com.lambdarookie` and adapt directories

---
+++ src/main/scala/com/lambdarookie/eventscala/publishers/Publisher.scala
@@ -0,0 +1,24 @@
+package com.lambdarookie.eventscala.publishers
+
+import akka.actor.{Actor, ActorRef}
+import Publisher._
+
+trait Publisher extends Actor {
+
+  var subscribers: Set[ActorRef] =
+    scala.collection.immutable.Set.empty[ActorRef]
+
+  override def receive: Receive = {
+    case Subscribe =>
+      subscribers = subscribers + sender()
+      sender() ! AcknowledgeSubscription
+  }
+
+}
+
+object Publisher {
+
+  case object Subscribe
+  case object AcknowledgeSubscription
+
+}
