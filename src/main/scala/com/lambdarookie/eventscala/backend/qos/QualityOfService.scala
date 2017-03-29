79b2e0e023b1d1dbef2fc3181754e84cb0184e33
onurv13 <m.onur_vural@hotmail.com>
2017-03-30 01:01:47 +0300

    - Convert QualityOfService to class - Add missing empty QoS

--- src/main/scala/com/lambdarookie/eventscala/backend/qos/QualityOfService.scala
+++ src/main/scala/com/lambdarookie/eventscala/backend/qos/QualityOfService.scala
@@ -1,19 +1,32 @@
 package com.lambdarookie.eventscala.backend.qos
 
-import com.lambdarookie.eventscala.backend.system.Host
+import com.lambdarookie.eventscala.backend.system.{Host, Operator}
 
 /**
   * Created by monur.
   */
-trait QualityOfService {
+class QualityOfService(source: Host, value: Long) {
+  def lower(limit: Long): Boolean = {
+    value < limit
+  }
 
+  def higher(limit: Long): Boolean = {
+    value > limit
+  }
+
+  def within(limit: Long): Boolean = {
+    value <= limit
+  }
 }
 
-case class Latency(host1: Host, host2: Host, latency: Long) extends QualityOfService {
+case class Frequency(source: Host, frequency: Long) extends QualityOfService(source, frequency)
 
+case class Latency(source: Host, destination: Host, latency: Long) extends QualityOfService(source, latency) {
+//  def this(operator: Operator, latency: Latency) = ???
 }
 
-case class Bandwidth(broker: Host, kbps: Long) {
+case class Proximity(source: Host, destination: Host, proximity: Long) extends QualityOfService(source, proximity)
 
-}
+case class Throughput(source: Host, destination: Host, throughput: Long) extends QualityOfService(source, throughput)
 
+case class Bandwidth(broker: Host, bandwidth: Long) extends QualityOfService(broker, bandwidth)
