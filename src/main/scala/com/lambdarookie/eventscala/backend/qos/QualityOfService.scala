fe64ed7a5954487e6d1fb31efea4583eff4a7396
onurv13 <m.onur_vural@hotmail.com>
2017-04-26 23:33:14 +0200

    Refine Condition and Demand classes

--- src/main/scala/com/lambdarookie/eventscala/backend/qos/QualityOfService.scala
+++ src/main/scala/com/lambdarookie/eventscala/backend/qos/QualityOfService.scala
@@ -4,21 +4,19 @@ package com.lambdarookie.eventscala.backend.qos
   * Created by monur.
   */
 trait QualityOfService
-class Condition(protected val _isFulfilled: Boolean) extends QualityOfService {
-  def isFulfilled = _isFulfilled
+class Condition(condition: Boolean) extends QualityOfService {
+  def isFulfilled = condition
 }
-class Demand(_isFulfilled: Boolean) extends Condition(_isFulfilled) {
-  private var condition: Option[Condition] = None
+class Demand(demand: Boolean) extends Condition(demand) {
   private var isConditionFulfilled: Boolean = true
 
   def when(newCondition: Condition): Demand = {
     isConditionFulfilled = newCondition.isFulfilled
-    condition = Some(newCondition)
     this
   }
 
   override def isFulfilled: Boolean = {
-    if(isConditionFulfilled) _isFulfilled
+    if(isConditionFulfilled) demand
     else true
   }
 }
