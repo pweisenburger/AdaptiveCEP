decb9f360c0c9536b39ea43765fb2b8035e867ef
Andre Pacak <andre.pacak@googlemail.com>
2018-09-05 13:23:11 +0200

    support usage of tuples and hlists in same query

--- src/main/scala/util/tuplehlistsupport.scala
+++ src/main/scala/util/tuplehlistsupport.scala
@@ -1,6 +1,6 @@
 package util
 
-import shapeless.ops.hlist.HKernelAux
+import shapeless.ops.hlist.{HKernelAux, Tupler}
 import shapeless.ops.{hlist, traversable, tuple}
 import shapeless.{Generic, HList, Nat}
 
@@ -13,28 +13,49 @@ object tuplehlistsupport {
     def apply(): Int
   }
 
-  object Length {
+  trait LowPriorityLength {
+    implicit def anyLength[T](): Length[T] = () => 1
+  }
 
-    implicit def hlistLength[T <: HList](implicit op: HKernelAux[T]): Length[T] = new Length[T] {
-      override def apply(): Int = op().length
-    }
+  object Length extends LowPriorityLength {
 
-    implicit def tupleLength[T <: Product, R <: HList](implicit gen: Generic.Aux[T, R], op: HKernelAux[R]): Length[T] = new Length[T] {
-      override def apply(): Int = op().length
-    }
+    implicit def hlistLength[T <: HList](implicit op: HKernelAux[T]): Length[T] =
+      () => op().length
+
+    implicit def tupleLength[T <: Product, R <: HList](implicit gen: Generic.Aux[T, R], op: HKernelAux[R]): Length[T] =
+      () => op().length
   }
 
   trait Prepend[A, B] {
     type Out
   }
 
-  object Prepend {
+  trait LowPriorityPrepend {
+
+    implicit def hlistTuplePrepend[A <: HList, B <: Product, BH <: HList, R <: HList](implicit
+        gen: Generic.Aux[B, BH],
+        prepend: hlist.Prepend.Aux[A, BH, R]): Prepend.Aux[A, B, R] =
+      new Prepend[A, B] {
+        type Out = R
+      }
+
+    implicit def tupleHListPrepend[A <: Product, B <: HList, BP <: Product, R <: Product](implicit
+        gen: Tupler.Aux[B, BP],
+        prepend: tuple.Prepend.Aux[A, BP, R]): Prepend.Aux[A, B, R] =
+      new Prepend[A, B] {
+        type Out = R
+      }
+
+  }
+
+  object Prepend extends LowPriorityPrepend {
     type Aux[A, B, Out0] = Prepend[A, B] { type Out = Out0 }
 
     implicit def hlistPrepend[A <: HList, B <: HList, R <: HList](implicit prepend: hlist.Prepend.Aux[A, B, R]): Aux[A, B, R] =
       new Prepend[A, B] {
         type Out = R
       }
+
 
     implicit def tuplePrepend[A <: Product, B <: Product, R <: Product](implicit prepend: tuple.Prepend.Aux[A, B, R]): Aux[A, B, R] =
       new Prepend[A, B] {
@@ -67,7 +88,26 @@ object tuplehlistsupport {
     type Out
   }
 
-  object JoinOnNat {
+  trait LowPriorityJoinOnNat {
+
+    implicit def hlistTupleJoinOnNat[L <: HList, R <: Product, RH <: HList, PosL <: Nat, PosR <: Nat, Out0 <: HList](implicit
+        gen: Generic.Aux[R, RH],
+        joinOn: hlists.JoinOnNat.Aux[L, RH, PosL, PosR, Out0]): JoinOnNat.Aux[L, R, PosL, PosR, Out0] =
+      new JoinOnNat[L, R, PosL, PosR] {
+        type Out = Out0
+      }
+
+    implicit def tupleHListJoinOnNat[L <: Product, LH <: HList, R <: HList, RP <: Product, PosL <: Nat, PosR <: Nat, Out0 <: Product, Out0H <: HList](implicit
+        genL: Generic.Aux[L, LH],
+        tupler: Generic.Aux[R, RP],
+        genOut0: Generic.Aux[Out0, Out0H],
+        joinOn: hlists.JoinOnNat.Aux[LH, R, PosL, PosR, Out0H]): JoinOnNat.Aux[L, R, PosL, PosR, Out0] =
+      new JoinOnNat[L, R, PosL, PosR] {
+        type Out = Out0
+      }
+  }
+
+  object JoinOnNat extends LowPriorityJoinOnNat {
     type Aux[L, R, PosL <: Nat, PosR <: Nat, Out0] = JoinOnNat[L, R, PosL, PosR] { type Out = Out0 }
 
     implicit def hlistJoinOnNat[L <: HList, R <: HList, PosL <: Nat, PosR <: Nat, Out0 <: HList](implicit
@@ -90,7 +130,27 @@ object tuplehlistsupport {
     type Out
   }
 
-  object Disjunct {
+  trait LowPriorityDisjunct {
+
+    implicit def hlistTupleDisjunct[A <: HList, B <: Product, BH <: HList, R <: HList](implicit
+        genB: Generic.Aux[B, BH],
+        disjunct: hlists.Disjunct.Aux[A, BH, R]): Disjunct.Aux[A, B, R] =
+      new Disjunct[A, B] {
+        type Out = R
+      }
+
+    implicit def tupleHListDisjunct[A <: Product, AH <: HList, B <: HList, BP <: Product, R <: Product, RH <: HList](implicit
+        genA: Generic.Aux[A, AH],
+        tupler: Tupler.Aux[B, BP],
+        genR: Generic.Aux[R, RH],
+        disjunct: hlists.Disjunct.Aux[AH, B, RH]): Disjunct.Aux[A, B, R] =
+      new Disjunct[A, B] {
+        type Out = R
+      }
+
+  }
+
+  object Disjunct extends LowPriorityDisjunct {
     type Aux[A, B, Out0] = Disjunct[A, B] { type Out = Out0 }
 
     implicit def hlistDisjunct[A <: HList, B <: HList, R <: HList](implicit
@@ -98,6 +158,7 @@ object tuplehlistsupport {
       new Disjunct[A, B] {
         type Out = R
       }
+
 
     implicit def tupleDisjunct[A <: Product, AH <: HList, B <: Product, BH <: HList, R <: Product, RH <: HList](implicit
         genA: Generic.Aux[A, AH],
@@ -116,19 +177,12 @@ object tuplehlistsupport {
   object FromTraversable {
     implicit def hlistFromTraversable[R <: HList](implicit
         fromTraversable: traversable.FromTraversable[R]): FromTraversable[R] =
-      new FromTraversable[R] {
-        override def apply(l: GenTraversable[_]): Option[R] = {
-          fromTraversable(l)
-        }
-      }
+      (l: GenTraversable[_]) => { fromTraversable(l) }
 
     implicit def tupleFromTraversable[P <: Product, R <: HList](implicit
         gen: Generic.Aux[P, R],
         fromTraversable: traversable.FromTraversable[R]): FromTraversable[P] =
-      new FromTraversable[P] {
-        override def apply(l: GenTraversable[_]): Option[P] =
-          fromTraversable(l).map { hlist => gen.from(hlist) }
-      }
+      (l: GenTraversable[_]) => fromTraversable(l).map { hlist => gen.from(hlist) }
 
   }
 }
