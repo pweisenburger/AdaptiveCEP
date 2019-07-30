389947e4d18111e1651cf2a31003152ac912924d
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-07-30 16:31:41 +0200

    serialization problem

--- src/main/scala/adaptivecep/privacy/TestingEncryption.scala
+++ src/main/scala/adaptivecep/privacy/TestingEncryption.scala
@@ -6,9 +6,10 @@ import akka.pattern.ask
 
 import scala.concurrent.duration._
 import crypto._
+import adaptivecep.publishers._
 import crypto.cipher._
 import crypto.dsl.Implicits._
-import adaptivecep.data.Events.{DecryptIntAndPrintRequest, EncryptIntRequest}
+import adaptivecep.data.Events._
 import adaptivecep.distributed.centralized.HostActorCentralized
 import akka.actor.{ActorRef, ActorSystem, Address, Deploy, Props}
 import akka.remote.RemoteScope
@@ -28,6 +29,9 @@ object TestingEncryption extends App {
     val host1: ActorRef = actorSystem.actorOf(Props[HostActorCentralized].withDeploy(Deploy(scope = RemoteScope(address1))), "Host" + "1")
 
     val cryptoActor: ActorRef = actorSystem.actorOf(Props[CryptoServiceActor].withDeploy(Deploy(scope = RemoteScope(address1))), "Crypto")
+
+    val publisher: ActorRef = actorSystem.actorOf(Props(RandomPublisher( id => Event1(id))).withDeploy(Deploy(scope = RemoteScope(address1))), "A")
+
 
     val cryptoSvc = new CryptoServiceWrapper(cryptoActor)
     val interpret = new CEPRemoteInterpreter(cryptoActor)
