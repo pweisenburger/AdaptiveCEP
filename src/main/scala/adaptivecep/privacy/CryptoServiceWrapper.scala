35c1f1eaa5d0983f0c101635e8ebbc55511f0415
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-07-30 15:32:46 +0200

    adding crypto service wrapper

---
+++ src/main/scala/adaptivecep/privacy/CryptoServiceWrapper.scala
@@ -0,0 +1,25 @@
+package adaptivecep.privacy
+
+import adaptivecep.data.Events.{DecryptIntAndPrintRequest, EncryptIntRequest}
+import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.util.Timeout
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import crypto._
+import crypto.cipher._
+
+case class CryptoServiceWrapper(cryptoActor: ActorRef) {
+  implicit val timeout = new Timeout(5 seconds)
+
+  def encryptInt(scheme: Scheme, in: Int): EncInt = {
+    val future = cryptoActor ? EncryptIntRequest(Comparable, in)
+    val result = Await.result(future, timeout.duration).asInstanceOf[EncInt]
+    result
+  }
+
+  def decryptAndPrint(in: EncInt): Unit = {
+    cryptoActor ! DecryptIntAndPrintRequest(in)
+  }
+}
--- src/main/scala/adaptivecep/privacy/TestingEncryption.scala
+++ src/main/scala/adaptivecep/privacy/TestingEncryption.scala
@@ -7,79 +7,18 @@ import akka.pattern.ask
 import scala.concurrent.duration._
 import crypto._
 import crypto.cipher._
-import crypto.dsl._
 import crypto.dsl.Implicits._
-import argonaut._
-import Argonaut._
-import adaptivecep.data.Events.{DecryptIntAndPrintRequest, EncryptIntRequest, Event1}
-import adaptivecep.distributed.centralized.AppRunnerCentralized.{actorSystem, address1}
-import adaptivecep.distributed.centralized.EncryptionContext.keyRing
+import adaptivecep.data.Events.{DecryptIntAndPrintRequest, EncryptIntRequest}
 import adaptivecep.distributed.centralized.HostActorCentralized
-import adaptivecep.distributed.operator.Operator
-import adaptivecep.publishers.RandomPublisher
 import akka.actor.{ActorRef, ActorSystem, Address, Deploy, Props}
 import akka.remote.RemoteScope
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.{Await, ExecutionContext}
-//import adaptivecep.publishers.RandomPublisher
-//import akka.actor.{ActorRef, ActorSystem, Address, Deploy, Props}
-//import akka.remote.RemoteScope
-import akka.serialization._
-//
-//case class PrivacyContext(keyRing: KeyRing, interpreter: LocalInterpreter)
-//
-//class EncIntWrapper(val json: String, val encType: String) extends Serializable {
-//
-//  def +(that: EncIntWrapper)(implicit pc: PrivacyContext): EncIntWrapper = {
-//    //decode this and that
-//    val self = EncIntWrapper.unapply(this).get
-//    val other = EncIntWrapper.unapply(that).get
-//    val result = pc.interpreter(self + other)
-//    EncIntWrapper(result)
-//  }
-//
-//  def _isEven() (implicit pc: PrivacyContext): Boolean ={
-//    val self = EncIntWrapper.unapply(this).get
-//    import crypto.dsl.isEven
-//    pc.interpreter( isEven( self ))
-//  }
-//
-//}
-//
-//object EncIntWrapper {
-//  def apply(encInt: EncInt): EncIntWrapper = encInt match {
-//    case p: PaillierEnc => new EncIntWrapper(PaillierEnc.encode(p).toString(), "PAILLIER")
-//    case e: ElGamalEnc => new EncIntWrapper(ElGamalEnc.encode(e).toString(), "ELGAMAL")
-//    case a: AesEnc => new EncIntWrapper(AesEnc.aesCodec.Encoder(a).toString(), "AES")
-//    case o: OpeEnc => new EncIntWrapper(OpeEnc.opeCodec.Encoder(o).toString(), "OPE")
-//  }
-//
-//  def unapply(arg: EncIntWrapper)(implicit privacyContext: PrivacyContext): Option[EncInt] =
-//    arg.encType match {
-//      case "PAILLIER" => PaillierEnc.decode(privacyContext.keyRing.pub.paillier).decodeJson(arg.json.parseOption.get).toOption
-//      case "ELGAMAL" => ElGamalEnc.decode(privacyContext.keyRing.pub.elgamal).decodeJson(arg.json.parseOption.get).toOption
-//      case "AES" => AesEnc.aesCodec.decodeJson( arg.json.parseOption.get ).toOption
-//      case "OPE" => OpeEnc.opeCodec.decodeJson(arg.json.parseOption.get).toOption
-//    }
-//
-//}
-//
-////
-////sealed trait Student extends Serializable
-////
-////case class Student1[A](id: A, name: String) extends Student {
-////}
-
+import scala.concurrent.Await
 
 object TestingEncryption extends App {
-
-
   override def main(args: Array[String]): Unit = {
-
-//    import scala.concurrent.ExecutionContext.Implicits.global
-    implicit val timeout = new Timeout(5 seconds)
 
     val file = new File("application.conf")
     val config = ConfigFactory.parseFile(file).withFallback(ConfigFactory.load()).resolve()
@@ -90,20 +29,16 @@ object TestingEncryption extends App {
 
     val cryptoActor: ActorRef = actorSystem.actorOf(Props[CryptoServiceActor].withDeploy(Deploy(scope = RemoteScope(address1))), "Crypto")
 
-    val one = cryptoActor ? EncryptIntRequest(Comparable, 1)
-    val two = cryptoActor ? EncryptIntRequest(Comparable, 2)
-
-    val oneEnc = Await.result(one, timeout.duration).asInstanceOf[EncInt]
-    val twoEnc = Await.result(two, timeout.duration).asInstanceOf[EncInt]
-
-    cryptoActor ! DecryptIntAndPrintRequest(oneEnc)
-    cryptoActor ! DecryptIntAndPrintRequest(twoEnc)
-
+    val cryptoSvc = CryptoServiceWrapper(cryptoActor)
     val interpreter = CEPRemoteInterpreter(cryptoActor)
 
-    val result = interpreter.interpret( oneEnc + twoEnc )
+    val oneEnc = cryptoSvc.encryptInt(Comparable, 1)
+    val twoEnc = cryptoSvc.encryptInt(Comparable, 2)
 
-      cryptoActor ! DecryptIntAndPrintRequest(result)
+    cryptoSvc.decryptAndPrint(oneEnc)
+    cryptoSvc.decryptAndPrint(twoEnc)
+    val result = interpreter.interpret(oneEnc + twoEnc)
+    cryptoSvc.decryptAndPrint(result)
 
 
   }
