ea54f32b6e6027c13f810b01123311982755f443
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-08-08 15:54:56 +0200

    cond server fix

--- src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
+++ src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
@@ -1,13 +1,22 @@
 package adaptivecep.privacy
 
+import adaptivecep.data.Events._
 import adaptivecep.privacy.sgx.EventProcessorClient
 
 object TestingRemoteObject {
   def main(args: Array[String]): Unit = {
     try {
 
+
+      def cond = (x: Int) => x >5
+
+      val condE = toFunEventBoolean(cond)
+
+      val input = Event1(5)
+
       val client = EventProcessorClient("13.80.151.52", 60000)
       client.lookupObject()
+      client.processEvent( condE,input )
     } catch {
       case e: Exception =>
         println(e.getMessage)
--- src/main/scala/adaptivecep/privacy/sgx/EventProcessorServiceImpl.scala
+++ src/main/scala/adaptivecep/privacy/sgx/EventProcessorServiceImpl.scala
@@ -1,5 +1,5 @@
 package adaptivecep.privacy.sgx
-import adaptivecep.data.Events
+import adaptivecep.data.Events._
 
 class EventProcessorServiceImpl extends EventProcessorServer {
   /**
@@ -9,6 +9,22 @@ class EventProcessorServiceImpl extends EventProcessorServer {
     * @throws java.rmi.RemoteException
     * @return whether the event correspond to the condition closure
     */
-  override def applyPredicate(cond: Events.Event => Boolean, input: Events.Event): Boolean =
-    cond(input)
+  override def applyPredicate(cond: Event => Boolean, input: Event): Boolean =
+    (cond,input) match {
+      case ( f: (Event1 => Boolean) , e: Event1) => f(e)
+      case ( f: (Event2 => Boolean) , e: Event2) => f(e)
+      case ( f: (Event3 => Boolean) , e: Event3) => f(e)
+      case ( f: (Event4 => Boolean) , e: Event4) => f(e)
+      case ( f: (Event5 => Boolean) , e: Event5) => f(e)
+      case ( f: (Event6 => Boolean) , e: Event6) => f(e)
+      case _ => sys.error("unexpected type!")
+    }
+
+
+
+
+
+
+
+
 }
