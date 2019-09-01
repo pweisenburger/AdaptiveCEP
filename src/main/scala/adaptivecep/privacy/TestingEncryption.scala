346bb1ea7c915eb7b35ed724983e033211847bc5
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-09-01 17:31:03 +0200

    fix bug

--- src/main/scala/adaptivecep/privacy/TestingEncryption.scala
+++ src/main/scala/adaptivecep/privacy/TestingEncryption.scala
@@ -92,11 +92,10 @@ object TestingEncryption extends App {
 
       implicit val pc: PrivacyContext = NoPrivacyContext
 
-    val normalQuery: Query1[Int] =
+    val normalQuery: Query2[Int,Int] =
       stream[Int]("A").
         join(stream[Int]("B"), slidingWindow(1.instances), slidingWindow(1.instances) ).
-        where((a,b) => a > b)
-        .dropElem1(frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
+        where((a,b) => a > b,frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
 
 //    val testQ = stream[Int]("A").or(stream[Double,String]("B"))
 //      .where()
