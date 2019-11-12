ae4bead89f01dccf340d5640e03d6e793635b4c2
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-11-12 18:03:48 +0100

    test phe no extra communication

--- src/main/scala/adaptivecep/privacy/PerformanceEvaluation.scala
+++ src/main/scala/adaptivecep/privacy/PerformanceEvaluation.scala
@@ -69,23 +69,27 @@ object PerformanceEvaluation extends App {
     val publisherA: ActorRef = actorSystem.actorOf(Props(EvaluationPublisher(id => Event1(MeasureEvent(java.util.UUID.randomUUID.toString, id)))).withDeploy(Deploy(scope = RemoteScope(address1))), "A")
     val publisherB: ActorRef = actorSystem.actorOf(Props(RandomPublisher(id => Event1(id * 2))).withDeploy(Deploy(scope = RemoteScope(address2))), "B")
     //
-    //    val cryptoActor: ActorRef = actorSystem.actorOf(Props[CryptoServiceActor].withDeploy(Deploy(scope = RemoteScope(address1))), "CryptoService")
-    //
-    //    val cryptoSvc = new CryptoServiceWrapper(cryptoActor)
-    //
-    //    val interpret = new CEPRemoteInterpreter(cryptoSvc)
-    //
+    val cryptoActor: ActorRef = actorSystem.actorOf(Props[CryptoServiceActor].withDeploy(Deploy(scope = RemoteScope(address1))), "CryptoService")
+
+    val cryptoSvc = new CryptoServiceWrapper(cryptoActor)
+
+    val interpret = new CEPRemoteInterpreter(cryptoSvc)
+
     //    val encOne: EncInt = Await.result(cryptoSvc.encrypt(Comparable)(1), Duration(5, TimeUnit.SECONDS))
     //    val encTwo: EncInt = Await.result(cryptoSvc.encrypt(Comparable)(2), Duration(5, TimeUnit.SECONDS))
     //
-    //    val query: Query2[MeasureEventEncPhe, EncInt] =
+    //    val query1: Query2[MeasureEventEncPhe, EncInt] =
     //      stream[MeasureEventEncPhe]("A").and(stream[EncInt]("B"))
     //        .where((x, y) => interpret(interpret(interpret(x.data * encTwo) + encOne) > y), frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
 
-    val query: Query2[MeasureEvent, Int] =
-      stream[MeasureEvent]("A").
-        and(stream[Int]("B"))
-        .where((x, y) => x.data * 2 + 1 > y, frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
+    val query2: Query2[MeasureEventEncPhe, EncInt] =
+      stream[MeasureEventEncPhe]("A").and(stream[EncInt]("B"))
+        .where((x, y) => interpret(x.data < y), frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
+
+    //    val query1: Query2[MeasureEvent, Int] =
+    //      stream[MeasureEvent]("A").
+    //        and(stream[Int]("B"))
+    //        .where((x, y) => x.data * 2 + 1 > y, frequency > ratio(3500.instances, 1.seconds) otherwise { nodeData => /*println(s"PROBLEM:\tNode `${nodeData.name}` emits too few events!")*/})
 
 
     val publishers: Map[String, ActorRef] = Map(
@@ -113,28 +117,28 @@ object PerformanceEvaluation extends App {
       * SGX enabled privacy
       */
 
-    val eventProcessorClient = EventProcessorClient("13.80.151.52", 60000)
-    val measureEventTransformer = EncDecTransformer(encryptMeasureEvent, decryptMeasureEvent)
-    implicit val sgxPrivacyContext: PrivacyContext = SgxPrivacyContext(
-      Set(TrustedHost(NodeHost(host1))), // Trusted hosts
-      eventProcessorClient,
-      Map("A" -> Event1Rule(measureEventTransformer), "B" -> Event1Rule(IntEventTransformer))
-    )
+//    val eventProcessorClient = EventProcessorClient("13.80.151.52", 60000)
+//    val measureEventTransformer = EncDecTransformer(encryptMeasureEvent, decryptMeasureEvent)
+//    implicit val sgxPrivacyContext: PrivacyContext = SgxPrivacyContext(
+//      Set(TrustedHost(NodeHost(host1))), // Trusted hosts
+//      eventProcessorClient,
+//      Map("A" -> Event1Rule(measureEventTransformer), "B" -> Event1Rule(IntEventTransformer))
+//    )
 
 
     /** *
       * PHE enabled privacy
       */
 
-    //    val measureEventSourceMapper = PheSourceTransformer(pheMapMeasureEvent)
-    //    implicit val phePrivacyContext: PrivacyContext = PhePrivacyContext(
-    //      cryptoSvc,
-    //      Map("A" -> Event1Rule(measureEventSourceMapper), "B" -> Event1Rule(IntPheSourceMapper))
-    //    )
+        val measureEventSourceMapper = PheSourceTransformer(pheMapMeasureEvent)
+        implicit val phePrivacyContext: PrivacyContext = PhePrivacyContext(
+          cryptoSvc,
+          Map("A" -> Event1Rule(measureEventSourceMapper), "B" -> Event1Rule(IntPheSourceMapper))
+        )
 
 
     val placement: ActorRef = actorSystem.actorOf(Props(PlacementActorCentralized(actorSystem,
-      query,
+      query2,
       publishers,
       publishersHosts,
       AverageFrequencyMonitorFactory(interval = 3000, logging = false),
