a4738f97d68e3156937574b44b3c11b6610b682f
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-11-13 16:26:37 +0100

    test sgx again

--- src/main/scala/adaptivecep/privacy/PerformanceEvaluation.scala
+++ src/main/scala/adaptivecep/privacy/PerformanceEvaluation.scala
@@ -115,20 +115,20 @@ object PerformanceEvaluation extends App {
     /** *
       * Normal operations with no privacy what so ever
       */
-    implicit val pc: PrivacyContext = NoPrivacyContext
+//    implicit val pc: PrivacyContext = NoPrivacyContext
 
 
     /** *
       * SGX enabled privacy
       */
 
-    //    val eventProcessorClient = EventProcessorClient("13.80.151.52", 60000)
-    //    val measureEventTransformer = EncDecTransformer(encryptMeasureEvent, decryptMeasureEvent)
-    //    implicit val sgxPrivacyContext: PrivacyContext = SgxPrivacyContext(
-    //      Set(TrustedHost(NodeHost(host1))), // Trusted hosts
-    //      eventProcessorClient,
-    //      Map("A" -> Event1Rule(measureEventTransformer), "B" -> Event1Rule(IntEventTransformer))
-    //    )
+    val eventProcessorClient = EventProcessorClient("13.80.151.52", 60000)
+    val measureEventTransformer = EncDecTransformer(encryptMeasureEvent, decryptMeasureEvent)
+    implicit val sgxPrivacyContext: PrivacyContext = SgxPrivacyContext(
+      Set(TrustedHost(NodeHost(host1))), // Trusted hosts
+      eventProcessorClient,
+      Map("A" -> Event1Rule(measureEventTransformer), "B" -> Event1Rule(IntEventTransformer))
+    )
 
 
     /** *
