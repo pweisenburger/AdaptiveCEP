e58a71e7945ec22c802327de0c2f7074f2338ea6
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-11-14 16:28:06 +0100

    testing the cond=>event

--- src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
+++ src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
@@ -8,21 +8,28 @@ object TestingRemoteObject {
     try {
 
 
-      def cond = (x: Int) => x > 2500
+      def cond = (x: Int) => {x > 2500}
 
       val condE = toFunEventBoolean(cond)
 
-      val client = EventProcessorClient("13.80.151.52", 60000)
-      val remoteObject = client.lookupObject()
-
+//      val client = EventProcessorClient("13.80.151.52", 60000)
+//      val remoteObject = client.lookupObject()
 
       (1 to 5000).foreach(i => {
         val input = Event1(i)
-        if (remoteObject.applyPredicate(condE, input)) {
+
+        if(condE(input)){
           println("condition satisfied for " + i)
-        } else {
+        }else{
           println("condition not satisfied "  + i)
         }
+
+//        if (remoteObject.applyPredicate(condE, input)) {
+//          println("condition satisfied for " + i)
+//        } else {
+//          println("condition not satisfied "  + i)
+//        }
+
       })
 
     } catch {
