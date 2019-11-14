9d35ac7bbc0526e9925add6b1a5c932b533c1b80
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-11-14 16:16:33 +0100

    test loop

--- src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
+++ src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
@@ -15,15 +15,15 @@ object TestingRemoteObject {
       val client = EventProcessorClient("13.80.151.52", 60000)
       val remoteObject = client.lookupObject()
 
-      val input = Event1(5)
-      if (remoteObject.applyPredicate(condE, input)) {
-        println("condition satisfied")
-      } else {
-        println("condition not satisfied")
-      }
-//      (1 to 5000).foreach(i => {
-//
-//      })
+
+      (1 to 5000).foreach(i => {
+        val input = Event1(i)
+        if (remoteObject.applyPredicate(condE, input)) {
+          println("condition satisfied")
+        } else {
+          println("condition not satisfied")
+        }
+      })
 
     } catch {
       case e: Exception =>
