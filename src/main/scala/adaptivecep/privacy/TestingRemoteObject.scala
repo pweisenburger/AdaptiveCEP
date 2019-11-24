2560af987df92ea7da005b87de9b2467f8d619b7
ahmad.korbesa <ahmad.korbesa@outlook.com>
2019-11-24 12:33:25 +0100

    test

--- src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
+++ src/main/scala/adaptivecep/privacy/TestingRemoteObject.scala
@@ -1,12 +1,12 @@
 package adaptivecep.privacy
 
 import adaptivecep.data.Events._
-import adaptivecep.privacy.ConversionRules.{Event1Rule, Event2Rule, IntEventTransformer, getEncryptedEvent}
+import adaptivecep.privacy.ConversionRules._
 import adaptivecep.privacy.encryption.{CryptoAES, Encryption}
 import adaptivecep.privacy.sgx.EventProcessorClient
 import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.{IvParameterSpec, PBEKeySpec, SecretKeySpec}
-
+import adaptivecep.privacy.shared.Custom._
 
 
 object TestingRemoteObject {
@@ -22,21 +22,25 @@ object TestingRemoteObject {
       implicit val encryption: Encryption = CryptoAES(skeySpec, iv)
 
 
-      def cond = (x: Int,y : Int) => {x + y > 2500}
+      def cond = (x: MeasureEvent, y: Int) => {
+        x.data + y > 2500
+      }
 
       val condE = toFunEventBoolean(cond)
 
       val client = EventProcessorClient("13.80.151.52", 60000)
       val remoteObject = client.lookupObject()
 
+      val measureEventTransformer = EncDecTransformer(encryptMeasureEvent, decryptMeasureEvent)
+
       (1 to 5000).foreach(i => {
 
-        val input = Event2(i,i)
-        val encEvent = getEncryptedEvent(input, Event2Rule(IntEventTransformer,IntEventTransformer))
+        val input = Event2(MeasureEvent("test", i), i)
+        val encEvent = getEncryptedEvent(input, Event2Rule(measureEventTransformer, IntEventTransformer))
         if (remoteObject.applyPredicate(condE, encEvent)) {
           println("condition satisfied for " + i)
         } else {
-          println("condition not satisfied "  + i)
+          println("condition not satisfied " + i)
         }
       })
 
